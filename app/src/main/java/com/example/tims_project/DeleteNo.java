293820104e59953730e4c4a501fa6e380bf80660
mdyousufh <yousufhossan2015@gmail.com>
2021-08-25 00:00:12 +0600

    All Empty Layout Included

---
+++ app/src/main/java/com/example/tims_project/DeleteNo.java
@@ -0,0 +1,14 @@
+package com.example.tims_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteNo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_no);
+    }
+}
---
+++ app/src/main/java/com/example/tims_project/PaymentRep.java
@@ -0,0 +1,14 @@
+package com.example.tims_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentRep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_rep);
+    }
+}
